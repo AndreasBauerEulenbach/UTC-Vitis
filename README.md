2f41d159e1d9b843034a26bbb7e1556a54fa5502
Andreas Bauer <andreas.bauer2@allianz.at>
2023-11-25 19:58:25 +0100

    Rename project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UTCVitisGJM
+# UTCVitisFE
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UTC-Vitis-GJM'`, () => {
+  it(`should have as title 'UTC-Vitis-FE'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UTC-Vitis-GJM');
+    expect(app.title).toEqual('UTC-Vitis-FE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('UTC-Vitis-GJM app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('UTC-Vitis-FE app is running!');
   });
 });
